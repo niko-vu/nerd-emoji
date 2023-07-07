3a6ccfea891e612149fb945f8a775ce8c3d8767e
fearoffun <480dunce@gmail.com>
2023-07-06 22:31:31 -0700

    added some files and edited routes and server etc

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,5 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-const uuid = require('../helpers/uuid');
 
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
@@ -11,14 +10,12 @@ notes.get('/', (req, res) => {
 notes.post('/', (req, res) => {
   console.log(req.body);
 
-  const { username, topic, note } = req.body;
+  const { title, text } = req.body;
 
   if (req.body) {
     const newNote = {
-      username,
-      note,
-      topic,
-      note_id: uuid(),
+      title,
+      text,
     };
 
     readAndAppend(newNote, './db/db.json');
@@ -29,3 +26,16 @@ notes.post('/', (req, res) => {
 });
 
 module.exports = notes;
+
+
+// The following HTML routes should be created:
+
+// GET /notes should return the notes.html file.
+
+// GET * should return the index.html file.
+
+// The following API routes should be created:
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
